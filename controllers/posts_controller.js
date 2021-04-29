f454f0e4dd599d2677b6ab54c8041bf53f97f5fb
Ryani <ryanid.@Yannis-MacBook-Air-3.local>
2021-04-29 16:27:14 -0400

    index working

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,24 +3,50 @@ const Post = require('../models/posts.js')
 const posts = express.Router()
 
 
-//SEED ROUTE FOR DEMO//
-posts.get('/demo/seed', (req, res) => {
-  Post.create(
-    [
-      {
-        title: "Here\'s an example",
-        message: "First post here. Let\'s see how this goes 😬"
-      },
-      {
-        message: "This post won\'t have a title"
-      },
-      {
-        title: "Random thought",
-        message: "Which letter is silent in the word 'Scent', the 'S' or the 'C'?"
-      }
-    ],
-    (err, data) => {
-      res.redirect('/posts')
-    }
-  )
+const seed = [
+  {
+    title: "Here\'s an example",
+    message: "First post here. Let\'s see how this goes 😬"
+  },
+  {
+    title: "",
+    message: "This post won\'t have a title"
+  },
+  {
+    title: "Random thought",
+    message: "Which letter is silent in the word Scent, the S or the C?"
+  }
+]
+
+// // NEW //
+// posts.get('/new', (req, res) => {
+//   res.render('posts/new.ejs')
+// })
+
+// INDEX //
+posts.get('/', (req, res) => {
+  Post.find({}, (err, allPosts) => {
+    res.render('posts/index.ejs', {
+      posts: allPosts
+    })
+
+  })
 })
+
+// SEED ROUTE FOR DEMO //
+posts.get('/demo/seed', (req, res) => {
+  console.log('seed route accessed');
+  for (i in seed) {
+    Post.create(seed[i], (err, data) => {
+        if (err){
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      }
+    )
+  }
+  res.redirect('/posts')
+})
+
+module.exports = posts;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 //___________________
-//Dependencies
+//DEPENDENCIES
 //___________________
 const express = require('express');
 const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const app = express ();
+const mongoose = require('mongoose');
+const app = express();
 const db = mongoose.connection;
 require('dotenv').config()
 //___________________
-//Port
+//PORT
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3003;
 //___________________
-//Database
+//DATABASE
 //___________________
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -27,23 +27,29 @@ db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 //___________________
-//Middleware
+//MIDDLEWARE
 //___________________
 //use public folder for static assets
 app.use(express.static('public'));
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: true }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: false}));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 //___________________
-// Routes
+//CONTROLLERS
+//___________________
+const postController = require('./controllers/posts_controller.js')
+app.use('/posts', postController)
+//___________________
+// ROUTES
 //___________________
 //localhost:3000
-// app.get('/' , (req, res) => {
-//   res.send('Hello World!');
-// });
+app.get('/' , (req, res) => {
+  // res.send('Hello World!');
+  res.redirect('/posts')
+});
 //___________________
-//Listener
+//LISTENER
 //___________________
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
