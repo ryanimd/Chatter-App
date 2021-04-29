f7f031d9e2274cadd941d11cde15b374db2d5bb5
Ryani <ryanid.@Yannis-MacBook-Air-3.local>
2021-04-28 22:18:08 -0400

    set up MVC

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/' , (req, res) => {
+//   res.send('Hello World!');
+// });
 //___________________
 //Listener
 //___________________
