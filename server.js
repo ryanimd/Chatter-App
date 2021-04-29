6179dae5e6a425814c9f3e2ee065374a2e97a342
Ryani <ryanid.@Yannis-MacBook-Air-3.local>
2021-04-28 22:50:00 -0400

    added Models folder

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 //use public folder for static assets
 app.use(express.static('public'));
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: true }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
